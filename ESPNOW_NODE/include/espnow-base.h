a4b6166bd98d7896b8c0651e71efb0af59d35be2
Thái <62926491+nhthai173@users.noreply.github.com>
2024-07-30 10:51:51 +0700

    init project

---
+++ ESPNOW_NODE/include/espnow-base.h
@@ -0,0 +1,182 @@
+//
+// Created by Thái Nguyễn on 25/7/24.
+//
+
+#ifndef ESPNOW_BASE_H
+#define ESPNOW_BASE_H
+
+#define ESPNOW_DEBUG
+
+#ifdef ESPNOW_DEBUG
+#define DEBUG_ESP_NOW(...) Serial.printf(__VA_ARGS__)
+#else
+#define DEBUG_ESP_NOW(...)
+#endif
+
+#include <Arduino.h>
+#include <vector>
+
+#ifdef ESP8266
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+#else // ESP32
+
+#include <WiFi.h>
+#include <esp_now.h>
+
+#endif
+
+
+
+#ifndef ESP_OK
+#define ESP_OK 0
+#endif
+
+
+
+uint8_t ESP_NOW_BROADCAST_ADDRESS[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+
+void addPeer(uint8_t *addr, uint8_t channel = 0) {
+    if (esp_now_add_peer(addr, ESP_NOW_ROLE_COMBO, channel, NULL, 0) != ESP_OK) {
+        DEBUG_ESP_NOW("Error adding peer");
+        return;
+    }
+    DEBUG_ESP_NOW("Added peer %02x:%02x:%02x:%02x:%02x:%02x\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+}
+
+
+bool compareMacAddress(const uint8_t *mac1, const uint8_t *mac2) {
+    if (mac1 == nullptr || mac2 == nullptr) {
+        return false;
+    }
+    for (int i = 0; i < 6; i++) {
+        if (mac1[i] != mac2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+class ENData {
+
+private:
+    uint16_t _id = 0;
+    String _buffer;
+
+public:
+
+    ENData() = default;
+
+    explicit ENData(uint8_t *buffer) {
+        _buffer = (char *) buffer;
+        int index = _buffer.indexOf("\r\n\r");
+        if (index == -1) {
+            DEBUG_ESP_NOW("Invalid data\n");
+            _buffer = "";
+            return;
+        }
+        _buffer = _buffer.substring(0, index + 2);
+        _id = getParam("uid").toInt();
+    }
+
+    explicit ENData(const String &data) {
+        _buffer = data;
+    }
+
+    void addParam(const String &key, const String &value) {
+        _buffer += key + ":" + value + "\r\n";
+    }
+
+    String getParam(const String &key) {
+        int index = _buffer.indexOf(key);
+        if (index == -1) {
+            return "";
+        }
+        int start = index + key.length() + 1;
+        int end = _buffer.indexOf("\r\n", start);
+        return _buffer.substring(start, end);
+    }
+
+    uint16_t id(uint16_t id = 0) {
+        if (id != 0) {
+            _id = id;
+        }
+        return _id;
+    }
+
+    String MAN(const String &value = "") {
+        if (value != "") {
+            addParam("MAN", value);
+        }
+        return getParam("MAN");
+    }
+
+    void send(uint8_t *addr) {
+        if (!_buffer.startsWith("uid:")) {
+            _buffer = "uid:" + String(_id) + "\r\n" + _buffer;
+            _buffer += "\r";
+        }
+        esp_now_send(addr, (uint8_t *) _buffer.c_str(), _buffer.length());
+        DEBUG_ESP_NOW("Sent %d bytes to %02x:%02x:%02x:%02x:%02x:%02x\n", _buffer.length(), addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+        DEBUG_ESP_NOW("------ Buffer ------\n");
+        DEBUG_ESP_NOW("%s", _buffer.c_str());
+        DEBUG_ESP_NOW("---- End Buffer ----\n");
+    }
+
+    const char* c_str() {
+        return _buffer.c_str();
+    }
+};
+
+
+
+
+class ENBase {
+
+};
+
+
+
+/* Helper */
+class ENSeperator {
+private:
+    std::vector<String> _seperated;
+public:
+
+    ENSeperator(const String& str, const String& sep) {
+        int lastIndex = 0;
+        int index = -1;
+        while(true) {
+            index = str.indexOf(sep, lastIndex);
+            if (index == -1) {
+                _seperated.push_back(str.substring(lastIndex));
+                break;
+            }
+            _seperated.push_back(str.substring(lastIndex, index));
+            lastIndex = index + sep.length();
+        }
+    }
+
+    unsigned int count() {
+        return _seperated.size();
+    }
+
+    void forEach(const std::function<void(String&)> &callback) {
+        for (auto &item : _seperated) {
+            callback(item);
+        }
+    }
+
+    String& operator[](int index) {
+        return _seperated[index];
+    }
+};
+
+
+#endif //ESPNOW_BASE_H
---
+++ ESPNOW_NODE/src/main.cpp
@@ -0,0 +1,438 @@
+//
+// #define RECEIVER
+//
+//
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+//
+//#ifdef RECEIVER
+//
+//uint32_t lastRecvMillis = 0;
+//
+//#else // sender
+//enum send_state_t {
+//  IDLE,
+//  SENDING,
+//  SENT,
+//  FAILED,
+//  SUCCESS
+//};
+//
+////uint8_t target_mac[] = {0x2C, 0x3A, 0xE8, 0x3D, 0xF3, 0x8D};
+// uint8_t target_mac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//uint16_t id = 0;
+//uint16_t lastSendId = 0;
+//send_state_t lastSendStatus = IDLE;
+//uint32_t lastSendMillis = 0;
+//// sender mac: 40:91:51:5A:5E:FA
+//
+//
+//void displaySending(uint32_t num)
+//{
+//  lastSendStatus = SENDING;
+//  display.clearDisplay();
+//  display.setCursor(0, 0);
+//  display.printf("Sending\n\n     %d", num);
+//  display.display();
+//}
+//
+//void displaySendSuccess()
+//{
+//  lastSendStatus = IDLE;
+//  display.fillRect(0, 0, 128, 16, BLACK); // clear first row
+//  display.display();
+//  display.setCursor(0, 0);
+//  display.print("Success");
+//  display.display();
+//}
+//
+//void displaySendFailed()
+//{
+//  lastSendStatus = IDLE;
+//  display.fillRect(0, 0, 128, 16, BLACK); // clear first row
+//  display.display();
+//  display.setCursor(0, 0);
+//  display.print("Failed");
+//  display.display();
+//}
+//
+//#endif // sender
+//
+//
+//void send_cb(uint8_t *mac_addr, uint8_t status) {
+//  Serial.printf("[%d] Send to %02x:%02x:%02x:%02x:%02x:%02x\n", status, mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+//  if (status != 0) {
+//#ifndef RECEIVER
+//    lastSendStatus = FAILED;
+//#endif
+//  }
+//}
+//
+//
+//void recv_cb(uint8_t *mac_addr, uint8_t *data, uint8_t len) {
+//  digitalWrite(LED_BUILTIN, LOW);
+//  Serial.printf("Received %d bytes from %02x:%02x:%02x:%02x:%02x:%02x\n", len, mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+//  espnow_data_t espnow_data(data);
+//  Serial.printf("ID: %d\n", espnow_data.id);
+//  Serial.printf("Data: %s\n", espnow_data.data.c_str());
+//#ifdef RECEIVER
+//  lastRecvMillis = millis();
+////  addPeer(mac_addr, 1);
+//  espnow_data_t ack;
+//  ack.id = espnow_data.id;
+//  ack.data = "ACK";
+//  if (esp_now_send(mac_addr, (uint8_t *)&ack, sizeof(ack)) != 0)
+//  {
+//    Serial.println("-> Error sending data");
+//  }
+//#else
+//  if (lastSendStatus != SENDING) return;
+//  if (espnow_data.id == lastSendId) {
+//    if (espnow_data.data == "ACK") {
+//      lastSendStatus = SUCCESS;
+//    } else {
+//      lastSendStatus = FAILED;
+//    }
+//  }
+//#endif
+//}
+//
+//
+//void setup()
+//{
+//  // button
+//  pinMode(12, INPUT_PULLUP);
+//  pinMode(LED_BUILTIN, OUTPUT);
+//  digitalWrite(LED_BUILTIN, HIGH);
+//
+//  // Start serial
+//  Serial.begin(115200);
+//  while (!Serial)
+//    delay(1000);
+//
+//  delay(5000);
+//
+//#ifndef RECEIVER
+//  // Start display
+//  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+//  {
+//    Serial.println(F("SSD1306 allocation failed"));
+//    for (;;)
+//      ;
+//  }
+//#endif
+//
+//  // init espnow
+//  Serial.print("ESP Board MAC Address:  ");
+//  Serial.println(WiFi.macAddress());
+//
+//  WiFi.mode(WIFI_STA);
+//  int esp_init_code = esp_now_init();
+//  Serial.printf("ESP-NOW init code: %d\n", esp_init_code);
+//  if (esp_init_code != 0)
+//  {
+//    Serial.println("Error initializing ESP-NOW");
+//    return;
+//  }
+//
+//  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+//  esp_now_register_send_cb(send_cb);
+//  esp_now_register_recv_cb(recv_cb);
+//
+//#ifndef RECEIVER
+//  // Add peer
+//  if (esp_now_add_peer(target_mac, ESP_NOW_ROLE_COMBO, 1, NULL, 0) != 0)
+//  {
+//    Serial.println("Error adding peer");
+//    return;
+//  }
+//
+//  // Initialize display
+//  display.clearDisplay();
+//  display.setTextSize(2);
+//  display.setTextColor(WHITE);
+//  // display.setRotation(0);
+//
+//  display.setCursor(0, 0);
+//  display.println("Ready");
+//  display.display();
+//#endif
+//
+//} // setup
+//
+//void loop()
+//{
+//#ifdef RECEIVER
+//
+//if (lastRecvMillis > 0 && millis() - lastRecvMillis > 3000)
+//{
+//  lastRecvMillis = 0;
+//  digitalWrite(LED_BUILTIN, HIGH);
+//}
+//
+//#else // sender
+//  if (digitalRead(12) == LOW)
+//  {
+//    delay(250);
+//     uint32_t num = millis() / 2000;
+//     espnow_data_t data;
+//     data.id = id++;
+//     lastSendId = data.id;
+//     data.data = String(num);
+//     displaySending(num);
+//     lastSendMillis = millis();
+//     if (esp_now_send(target_mac, (uint8_t *)&data, sizeof(data)) != 0)
+//     {
+//       Serial.println("Error sending data");
+//       displaySendFailed();
+//     }
+//  }
+//   if (lastSendStatus == SUCCESS)
+//   {
+//     displaySendSuccess();
+//   }
+//   else if (lastSendStatus == FAILED)
+//   {
+//     displaySendFailed();
+//   }
+//   else if (lastSendStatus == SENDING && lastSendMillis > 0 && millis() - lastSendMillis > 5000)
+//   {
+//     lastSendMillis = 0;
+//     displaySendFailed();
+//   }
+//#endif
+//}
+
+
+
+#define NODE
+
+
+
+#include "Arduino.h"
+
+// 2e:3a:e8:3d:f3:8d
+uint8_t gw_mac[] = {0x2C, 0x3A, 0xE8, 0x3D, 0xF3, 0x8D};
+uint8_t node_mac[] = {0x40, 0x91, 0x51, 0x5A, 0x5E, 0xFA};
+
+
+#ifdef NODE
+
+#include <espnow-node.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "OneButton.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+uint32_t lastMillis = 0;
+
+ENNodeInfo NodeInfo;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+OneButton button(12, true);
+
+void setup() {
+    // IO
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Start serial
+    Serial.begin(115200);
+    while (!Serial)
+        delay(1000);
+
+    delay(5000);
+
+    // Start display
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;);
+    }
+
+    // init espnow
+    Serial.print("ESP Board MAC Address:  ");
+    Serial.println(WiFi.macAddress());
+
+    WiFi.mode(WIFI_STA);
+    wifi_set_channel(11);
+    NodeBegin();
+
+    WiFi.printDiag(Serial);
+
+    // Initialize display
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    // display.setRotation(0);
+
+    display.setCursor(0, 0);
+    display.println("Ready");
+    display.display();
+
+    button.attachLongPressStart([]() {
+        Serial.println("Enter pairing mode");
+        Node.startPairing();
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("Pairing");
+        display.display();
+    });
+
+    NodeInfo.name = "Node1";
+    NodeInfo.type = "ESP8266";
+    NodeInfo.model = "security-board";
+    NodeInfo.fw_ver = "1";
+    NodeInfo.support = "get_props,get_prop,set_prop";
+    NodeInfo.addProp(
+            "led",
+            "false",
+            []() {
+                return String(digitalRead(LED_BUILTIN) == LOW ? "true" : "false");
+            },
+            [](const String& value) {
+                if (value == "true")
+                    digitalWrite(LED_BUILTIN, LOW);
+                else if (value == "false")
+                    digitalWrite(LED_BUILTIN, HIGH);
+                else
+                    return false;
+                return true;
+            });
+
+    Node.setNodeInfo(&NodeInfo);
+
+    Node.setGatewayMac(gw_mac);
+
+    Node.onPairingTimeout([]() {
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.printf("Pairing\ntimeout");
+        display.display();
+    });
+
+    Node.onPairingSuccess([]() {
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.printf("Pairing\nsuccess");
+        display.display();
+    });
+
+    esp_now_register_send_cb([](uint8_t *mac, uint8_t status) {
+        Serial.printf("[%d] Send to %02x:%02x:%02x:%02x:%02x:%02x\n", status, mac[0], mac[1], mac[2], mac[3], mac[4],
+                      mac[5]);
+    });
+
+} // setup
+
+
+void loop() {
+    Node.loop();
+    button.tick();
+
+//    if (millis() - lastMillis > 20000) {
+//        lastMillis = millis();
+//        display.clearDisplay();
+//        display.setCursor(0, 0);
+//        display.printf("Sending sync\n%lu", millis());
+//        display.display();
+//        Node.sendSyncProps();
+//    }
+}
+
+#endif
+
+
+#ifndef NODE
+
+#include <espnow-gateway.h>
+#include "OneButton.h"
+#include "ESP8266WebServer.h"
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+OneButton button(0, true);
+
+ESP8266WebServer sv(80);
+
+void setup() {
+    // IO
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Start serial
+    Serial.begin(115200);
+    while (!Serial)
+        delay(1000);
+
+    delay(5000);
+
+    Serial.print("ESP Board MAC Address:  ");
+    Serial.println(WiFi.macAddress());
+
+    wifi_get_macaddr(STATION_IF, gw_mac);
+    Serial.printf("Gateway MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", gw_mac[0], gw_mac[1], gw_mac[2], gw_mac[3], gw_mac[4],
+                  gw_mac[5]);
+
+    WiFi.mode(WIFI_AP_STA);
+    Serial.print("Connecting to WiFi..");
+    WiFi.begin("C21.20", "diamondc2120");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("Connected");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("RSSI: ");
+    Serial.println(WiFi.RSSI());
+    Serial.print("Channel: ");
+    Serial.println(WiFi.channel());
+
+    // init espnow
+    wifi_set_macaddr(STATION_IF, gw_mac);
+    GatewayBegin();
+
+    button.attachLongPressStart([]() {
+        Serial.println("Enter pairing mode");
+        Gateway.startPairing();
+    });
+
+//    Node.onPairingTimeout([](){
+//        display.clearDisplay();
+//        display.setCursor(0, 0);
+//        display.printf("Pairing\ntimeout");
+//        display.display();
+//    });
+
+    Gateway.onPairingRequest([](uint8_t *mac) {
+        digitalWrite(LED_BUILTIN, LOW);
+    });
+
+//    addPeer(node_mac);
+
+    sv.on("/", []() {
+        sv.send(200, "text/plain", "Hello World");
+    });
+
+    sv.on("/command", []() {
+        String action = sv.arg("action");
+        String params = sv.arg("params");
+        Gateway.sendCommand(node_mac, action, params);
+        sv.send(200, "text/plain", "OK");
+    });
+
+    sv.begin();
+
+} // setup
+
+
+void loop() {
+    Gateway.loop();
+    button.tick();
+    sv.handleClient();
+}
+
+#endif
